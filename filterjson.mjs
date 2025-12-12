2915ab636e7af3c8d69f222fa5e3df221d5cc27d
Ziffix <125783558+Ziffixture@users.noreply.github.com>
2025-12-12 15:18:13 -0700

    Update filterjson.mjs

--- filterjson.mjs
+++ filterjson.mjs
@@ -1,46 +1,61 @@
+const DEFAULT_EXHAUST_FIELDS = {
+    parameter : "cursor", 
+    property  : "nextPageCursor", 
+    extract   : (body) => body.data,
+}
+
+
+
+export function getMarketInfo(item) {
+    return {
+        id    : item.id,
+        name  : item.name,
+        price : item.price,
+    }
+}
+
+export function getIndentificationInfo(item) {
+    return {
+        id   : item.id,
+        name : item.name,
+    }
+}
+
+
 export default async function filterJSON(parameters) {
     let results = []
 
+    const exhaust      = parameters.exhaust
+    const exhaustField = parameters.exhaustFields ?? DEFAULT_EXHAUST_FIELDS
+        
+    const url    = parameters.url
+    const filter = parameters.filter
+
+    let cursor = ""
+
     try {
-        let cursor = ""
-
-        const response = await fetch(parameters.url + `&cursor=${cursor}`)
-        if (!response.ok) {
-            return
-        }
-
-        const body = await response.json()
-
-        for (const row of body.data) {
-            const result = parameters.filter(row)
-            if (result) {
-                results.push(result)
+        do {
+            const response = await fetch(`${url}&${exhaustField.parameter}=${cursor}`)
+            if (!response.ok) {
+                return
             }
-        }
 
-        if (body.nextPageCursor && parameters.exhaust) {
-            cursor = body.nextPageCursor
-        }
+            const body = await response.json()
+            const data = exhaustField.extract(body)
 
+            for (const row of data) {
+                const result = filter(row)
+                if (result) {
+                    results.push(result)
+                }
+            }
+
+            cursor = exhaust && body[exhaustField.property]
+        } while (cursor)
     } 
     catch (error) {
         console.log(error)
     }
 
     return results
-}
-
-export function getMarketInfo(item) {
-    return {
-        id: item.id,
-        name: item.name,
-        price: item.price,
-    }
-}
-
-export function getIndentificationInfo(item) {
-    return {
-        id: item.id,
-        name: item.name,
-    }
 }
