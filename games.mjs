54a0d4a8e5eaff3fed582f25d3215b46cacb0171
Ziffix <125783558+Ziffixture@users.noreply.github.com>
2025-12-12 15:18:23 -0700

    Update games.mjs

--- games.mjs
+++ games.mjs
@@ -1,43 +1,53 @@
 import filterJSON, { getMarketInfo, getIndentificationInfo } from "./filterjson.mjs"
 
 
-const Games = {}
 
 const CreatorTypes = {
-    User: "User",
-    Group: "Group",
+    User  : "User",
+    Group : "Group",
 }
+
+const CREATOR_TYPE_URIS = {
+    [CreatorTypes.User]  : "users",
+    [CreatorTypes.Group] : "groups",
+}
+
+const GAME_PASS_EXHAUST_FIELDS = {
+    parameter : "pageToken", 
+    property  : "nextPageToken",
+    extract   : (body) => body.gamePasses,
+}
+
+const Games = {}
+
 
 
 Games.get = async function(creatorId, creatorType) {
-    const creatorTypeUris = {
-        [CreatorTypes.User]: "users",
-        [CreatorTypes.Group]: "groups",
-    }
-    
-    const creatorTypeUri = creatorTypeUris[creatorType]
+    const creatorTypeUri = CREATOR_TYPE_URIS[creatorType]
     if (!creatorTypeUri) {
         throw new Error("Unkown creator type.")
     }
     
-    const games = await filterJSON({
-        url: `https://games.roblox.com/v2/${creatorTypeUri}/${creatorId}/games?accessFilter=2&limit=50&sortOrder=Asc`,
-        exhaust: true,
-        filter: getIndentificationInfo,
-    })
+    const parameters = {}
 
-    return games
+    parameters.url     = `https://games.roblox.com/v2/${creatorTypeUri}/${creatorId}/games?accessFilter=2&limit=50&sortOrder=Asc`
+    parameters.filter  = getIndentificationInfo
+    parameters.exhaust = true
+
+    return await filterJSON(parameters)
 }
 
 Games.getPasses = async function(universeId) {
-    const passes = await filterJSON({
-        url: `https://games.roblox.com/v1/games/${universeId}/game-passes?limit=10&sortOrder=1`,
-        exhaust: true,
-        filter: getMarketInfo,
-    })
+    const parameters = {}
 
-    return passes
+    parameters.url           = `https://apis.roblox.com/game-passes/v1/universes/${universeId}/game-passes?passView=Full&pageSize=50`
+    parameters.filter        = getMarketInfo
+    parameters.exhaust       = true
+    parameters.exhaustFields = GAME_PASS_EXHAUST_FIELDS
+
+    return await filterJSON(parameters)
 }
+
 
 
 export default Games
